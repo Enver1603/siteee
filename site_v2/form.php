0fa23ffd71e0923be6e2ba99f068d65154002293
Enver1603 <163664554+Enver1603@users.noreply.github.com>
2024-07-11 19:39:53 +0300

    son1

---
+++ site_v2/form.php
@@ -0,0 +1,13 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'php_mailer/src/Exception.php';
+require 'php_mailer/src/PHPMailer.php';
+require 'php_mailer/src/SMTP.php';
+
+
+
+
+
+?>
